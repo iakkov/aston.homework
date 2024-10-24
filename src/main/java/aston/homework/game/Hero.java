c39872118558bf22b2bd9e33e190ddea02647aca
iakovlysenko <lysenko_iakov@yahoo.com>
2024-10-24 11:20:42 +0300

    Add setter and getted for a name

--- src/main/java/aston/homework/game/Hero.java
+++ src/main/java/aston/homework/game/Hero.java
@@ -1,4 +1,12 @@
 package aston.homework.game;
 
 public class Hero {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
 }
